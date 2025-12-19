dbc95f8052010d253bf06b1007a68e8f746c5126
Armin Imani <arminimani70@gmail.com>
2025-12-20 00:44:37 +0100

    Initial commit: Python CLI To-Do app

---
+++ README.md
@@ -0,0 +1,16 @@
+# 📝 Python CLI To-Do App
+
+A Python command-line To-Do app with clean architecture and JSON storage.
+
+## Features
+
+- Add new tasks
+- Show all tasks with status
+- Mark tasks as done
+- Persistent storage using JSON
+
+## How to Run
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,111 @@
+import json
+import os
+
+
+def load_tasks(filename):
+    if not os.path.exists(filename):
+        return []
+
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+def save_tasks(tasks, filename):
+    with open(filename, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def show_menu():
+    print("\n--- TO DO MENU ---")
+    print("1. Add a new task")
+    print("2. Show tasks")
+    print("3. Mark task as done")
+    print("4. Exit")
+
+    return input("Choose an option: ")
+
+
+def add_task():
+    title = input("Enter your task: ")
+    return title
+
+
+def show_tasks(tasks):
+    if not tasks:
+        print("Task list is empty.")
+        return
+
+    for task in tasks:
+        status = "Done" if task["completed"] else " "
+        print(f'{task["id"]}. {task["title"]} [{status}]')
+
+
+def mark_task_done(tasks):
+    if not tasks:
+        print("Task list is empty.")
+        return
+
+    show_tasks(tasks)
+
+    while True:
+        user_input = input("Enter task ID to mark as done (0 = exit): ")
+
+        if user_input == "0":
+            break
+
+        task_id = int(user_input)
+        found = False
+
+        for task in tasks:
+            if task["id"] == task_id:
+                task["completed"] = True
+                print("Task marked as done.")
+                found = True
+                break
+
+        if found:
+            break
+        else:
+            print("Task not found.")
+
+
+def main():
+    filename = "tasks.json"
+    tasks = load_tasks(filename)
+
+    while True:
+        choice = show_menu()
+
+        if choice == "1":
+            title = add_task()
+
+            if not tasks:
+                new_id = 1
+            else:
+                new_id = max(task["id"] for task in tasks) + 1
+
+            task = {
+                "id": new_id,
+                "title": title,
+                "completed": False
+            }
+
+            tasks.append(task)
+
+        elif choice == "2":
+            show_tasks(tasks)
+
+        elif choice == "3":
+            mark_task_done(tasks)
+
+        elif choice == "4":
+            save_tasks(tasks, filename)
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option.")
+
+
+if __name__ == "__main__":
+    main()
